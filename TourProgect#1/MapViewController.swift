33a27ee6607c9b557f6b24bb9f229c07612753fa
pavlokharambura <pasha.harambura@gmail.com>
2017-07-27 20:00:12 +0300

    ForAntor

--- TourProgect#1/MapViewController.swift
+++ TourProgect#1/MapViewController.swift
@@ -12,19 +12,16 @@ import GoogleMaps
 class MapViewController: UIViewController, IndicatorInfoProvider {
     
    
+    @IBOutlet weak var MyMapView: GMSMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         
-        GMSServices.provideAPIKey("AIzaSyDI_ru8PsfByOfydUyomBieJfEVBIIiwQs")
         
         let camera = GMSCameraPosition.camera(withLatitude: 46.467694, longitude: 30.705600, zoom: 10)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.isMyLocationEnabled = true
-        view = mapView
-        
+        MyMapView.camera = camera
 
         
         // Do any additional setup after loading the view.
