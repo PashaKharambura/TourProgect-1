e16ed2c454afd8f26f135670a56a3a6f1f6e92ea
antonpobigai <antonpobigai@gmail.com>
2017-07-27 21:51:45 +0300

    Pinch to zoom and Markers

--- TourProgect#1/MapViewController.swift
+++ TourProgect#1/MapViewController.swift
@@ -9,17 +9,45 @@
 import XLPagerTabStrip
 import GoogleMaps
 
+struct Place {
+    let name: String
+    let long: CLLocationDegrees
+    let lat: CLLocationDegrees
+}
+
+
+let Places = [
+    Place(name: "Odessa", long: 30.705600, lat: 46.467694),
+    Place(name: "Shoto", long: 30.705691, lat: 46.467041),
+    Place(name: "Gdeto", long: 30.705800, lat: 46.467041),
+    
+]
+
+var markerDict: [String: GMSMarker] = [:]
+
 class MapViewController: UIViewController, IndicatorInfoProvider {
+    
+    @IBOutlet weak var MyMapView: GMSMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        GMSServices.provideAPIKey("AIzaSyDI_ru8PsfByOfydUyomBieJfEVBIIiwQs")
+        
+        
+        let PinchGesture = UIPinchGestureRecognizer(target: self, action: "PinchGesture")
+        self.MyMapView.addGestureRecognizer(PinchGesture)
+        
         
         let camera = GMSCameraPosition.camera(withLatitude: 46.467694, longitude: 30.705600, zoom: 10)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.isMyLocationEnabled = true
-        view = mapView
+        MyMapView.camera = camera
+        
+        for Place in Places {
+            let Place_marker = GMSMarker()
+            Place_marker.position = CLLocationCoordinate2D(latitude: Place.lat, longitude: Place.long)
+            Place_marker.title = Place.name
+            Place_marker.map = MyMapView
+            markerDict[Place.name] = Place_marker
+        }
         
 
         
@@ -30,5 +58,51 @@ class MapViewController: UIViewController, IndicatorInfoProvider {
         return IndicatorInfo(title: "Карта")
     }
     
+    func PinchGesture(sender: UIPinchGestureRecognizer) {
+        sender.view!.transform = sender.view!.transform.scaledBy(x: sender.scale, y: sender.scale)
+        sender.scale = 1
+    }
+    
     
 }
+/*
+
+ class MapViewController: UIViewController, IndicatorInfoProvider {
+ 
+ 
+ 
+ let PinchGesture = UIPinchGestureRecognizer(target: self, action: "PinchGesture")
+ self.MyMapView.addGestureRecognizer(PinchGesture)
+ 
+ 
+ let camera = GMSCameraPosition.camera(withLatitude: 46.467694, longitude: 30.705600, zoom: 10)
+ MyMapView.camera = camera
+ 
+ for Place in Places {
+ let Place_marker = GMSMarker()
+ Place_marker.position = CLLocationCoordinate2D(latitude: Place.lat, longitude: Place.long)
+ Place_marker.title = Place.name
+ Place_marker.map = MyMapView
+ markerDict[Place.name] = Place_marker
+ }
+ 
+ 
+ 
+ 
+ 
+ // Do any additional setup after loading the view.
+ }
+ 
+ func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+ return IndicatorInfo(title: "Карта")
+ }
+ 
+ func PinchGesture(sender: UIPinchGestureRecognizer) {
+ sender.view!.transform = sender.view!.transform.scaledBy(x: sender.scale, y: sender.scale)
+ sender.scale = 1
+ }
+ 
+ }
+
+ 
+ */
