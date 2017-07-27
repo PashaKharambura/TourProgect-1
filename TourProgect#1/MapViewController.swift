eb85c22221488b268e36542ea2913efd5ce32cf0
pavlokharambura <pasha.harambura@gmail.com>
2017-07-27 20:45:41 +0300

    AddingMarkers

--- TourProgect#1/MapViewController.swift
+++ TourProgect#1/MapViewController.swift
@@ -23,8 +23,22 @@ class MapViewController: UIViewController, IndicatorInfoProvider {
         let camera = GMSCameraPosition.camera(withLatitude: 46.467694, longitude: 30.705600, zoom: 10)
         MyMapView.camera = camera
 
+        let marker1 = GMSMarker()
+        marker1.position = CLLocationCoordinate2DMake(46.467694, 30.705600)
+        marker1.title = "first"
+        let marker2 = GMSMarker()
+        marker2.position = CLLocationCoordinate2DMake(46.47, 30.8)
+        marker2.title = "second"
         
-        // Do any additional setup after loading the view.
+        let markers: [GMSMarker] = [marker1, marker2]
+        
+        addMarker(markers: markers)
+    }
+    
+    func addMarker (markers: [GMSMarker]) {
+        for marker in markers {
+            marker.map = self.MyMapView
+        }
     }
     
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
