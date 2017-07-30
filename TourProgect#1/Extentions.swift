cc52f38f44f8ce16b9a09f0e62110c49c56ead5b
pavlokharambura <pasha.harambura@gmail.com>
2017-07-30 19:34:31 +0300

    login screen

--- TourProgect#1/Extentions.swift
+++ TourProgect#1/Extentions.swift
@@ -49,3 +49,43 @@ extension UITextField {
     }
     
 }
+
+extension String {
+    
+    var length: Int {
+        return self.characters.count
+    }
+    
+    subscript (i: Int) -> String {
+        return self[Range(i ..< i + 1)]
+    }
+    
+    func substring(from: Int) -> String {
+        return self[Range(min(from, length) ..< length)]
+    }
+    
+    func substring(to: Int) -> String {
+        return self[Range(0 ..< max(0, to))]
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return self[Range(start ..< end)]
+    }
+    
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
--- TourProgect#1/LogInViewController.swift
+++ TourProgect#1/LogInViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var firstTextField: UITextField!
     @IBOutlet weak var secondTextField: UITextField!
     @IBOutlet weak var thirdTextField: UITextField!
@@ -16,12 +16,55 @@ class LogInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-    }
+        firstTextField.delegate = self
+        secondTextField.delegate = self
+        thirdTextField.delegate = self
+        
+        firstTextField.becomeFirstResponder()
+        self.hideKeyboardWhenTappedAround()
 
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if (string == UIPasteboard.general.string) {
+            firstTextField.text = string[0]+string[1]
+            secondTextField.text = string[2]+string[3]
+            thirdTextField.text = string[4]+string[5]
+        }
+        
+        if firstTextField.text?.characters.count == 2 && string != "" {
+            secondTextField.becomeFirstResponder()
+        }
+        
+        if secondTextField.text?.characters.count == 2 && string != "" {
+            thirdTextField.becomeFirstResponder()
+        }
+        
+        if secondTextField.text?.length == 0 && string == "" {
+            firstTextField.becomeFirstResponder()
+        }
+        
+        let  char = string.cString(using: String.Encoding.utf8)!
+        let isBackSpace = strcmp(char, "\\b")
+        
+        if (isBackSpace == -92) {
+            firstTextField.text = ""
+            secondTextField.text = ""
+            thirdTextField.text = ""
+            firstTextField.becomeFirstResponder()
+        }
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-
     
 }
